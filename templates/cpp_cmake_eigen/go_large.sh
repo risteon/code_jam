1fb6b0687d6b8d12b12ff42ee6e5fabf8d6aa6c1
Christoph Rist <rist.christoph@gmail.com>
2016-06-20 17:06:56 +0200

    Add new template using Eigen with cmake

---
+++ templates/cpp_cmake_eigen/go_large.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+build/jam.out < *-large-practice.in > large-solution.out
---
+++ templates/cpp_cmake_eigen/template.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <cmath>
+#include <climits>
+#include <numeric>
+
+#include <Eigen/Dense>
+
+#undef N_DEBUG
+#include <assert.h>
+
+using namespace std;
+
+using UInt = uint64_t;
+using Int = int64_t;
+using Vector3d = Eigen::Matrix<double, 3, 1>;
+
+struct Thing
+{
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+  Vector3d position;
+  Vector3d velocity;
+};
+
+
+int main()
+{
+  size_t num_test_cases;
+  cin >> num_test_cases;
+
+  for (size_t i = 0; i < num_test_cases; i++)
+  {
+    size_t vs;
+    cin >> vs;
+
+
+    std::cout <<"Case #" <<(i+1) <<": "  <<endl;
+
+  }
+  
+  return 0;
+}
