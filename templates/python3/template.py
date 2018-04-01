47ffe09dd06d77802612848e9674bf92c2439ebb
Christoph Rist <c.rist@posteo.de>
2018-04-01 13:10:43 +0200

    Add python3 template file

---
+++ templates/python3/template.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    t = int(sys.stdin.readline())
+    
+    # numbers
+    a, b, c = map(int, sys.stdin.readline().strip().split(" "))
+
+
+if __name__ == "__main__":
+    main()
+
